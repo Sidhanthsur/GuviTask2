722b28af891ca62941828c1f9092714d67fbc7de
sidhanthsurana <sidhanth.surana12@gmail.com>
2016-09-22 22:28:11 +0530

    safe

--- app/src/main/java/surana/sidhanth/com/guvitask/model/item.java
+++ app/src/main/java/surana/sidhanth/com/guvitask/model/item.java
@@ -7,9 +7,40 @@ package surana.sidhanth.com.guvitask.model;
 public class item {
 
     String name;
-    String price;
+    int price;
     String description;
     String url;
 
-
+    public void setName(String s)
+    {
+        this.name = s;
+    }
+    public void setPrice(int s)
+    {
+        this.price = s;
+    }
+    public void setDescription(String description2)
+    {
+        this.description = description2;
+    }
+    public void setUrl(String s)
+    {
+        this.url = s;
+    }
+    public String getName()
+    {
+        return name;
+    }
+    public int getPrice()
+    {
+        return price;
+    }
+    public String getDescription()
+    {
+        return description;
+    }
+    public String getUrl()
+    {
+        return url;
+    }
 }
