116389e31c101d5af0be9df19dfb3ad156b42a80
sidhanthsurana <sidhanth.surana12@gmail.com>
2016-09-22 22:25:30 +0530

    safe

---
+++ app/src/main/java/surana/sidhanth/com/guvitask/ParseJson.java
@@ -0,0 +1,22 @@
+package surana.sidhanth.com.guvitask;
+
+/**
+ * Created by sidhanth on 22/9/16.
+ */
+
+public class ParseJson {
+
+    String json = "[{\"_id\":\"57c928fb208e95cc2d30e779\",\"item_name\":\"Chicken\n" +
+            "Coffee\",\"item_description\":\"Ada Chicken la\n" +
+            "coffee\",\"item_type\":\"Coffee\",\"item_status\":2,\"toast_price\":250,\"restaurant\":\"572afada57f0798a07e1ee07\",\"__v\":0,\"active\":true,\"item_image_url_path\":[\"/home/toast2/toast/public/uploads/1472114407898Humus-&-Pita-.jpg\"],\"item_image_url\":[\"http://toastcentral.com/uploads/1472114407898Humus-&-Pita-.jpg\"]},{\"_id\":\"57c92abc6742c38402b51757\",\"item_name\":\"Chicken\n" +
+            "Soup\",\"item_description\":\"Ada Chicken la\n" +
+            "soup\",\"item_type\":\"Soup\",\"item_status\":2,\"toast_price\":250,\"restaurant\":\"572afada57f0798a07e1ee07\",\"__v\":0,\"active\":true,\"item_image_url_path\":[\"/home/toast2/toast/public/uploads/1472114407898Humus-&-Pita-.jpg\"],\"item_image_url\":[\"http://toastcentral.com/uploads/1472114407898Humus-&-Pita-.jpg\"]},{\"_id\":\"57c92af46742c39202b51756\",\"item_name\":\"Chicken\n" +
+            "Rice\",\"item_description\":\"Ada Chicken la rice\",\"item_type\":\"Main\n" +
+            "Dish\",\"item_status\":2,\"toast_price\":250,\"restaurant\":\"572afada57f0798a07e1ee07\",\"__v\":0,\"active\":true,\"item_image_url_path\":[\"/home/toast2/toast/public/uploads/1472114407898Humus-&-Pita-.jpg\"],\"item_image_url\":[\"http://toastcentral.com/uploads/1472114407898Humus-&-Pita-.jpg\"]},{\"_id\":\"57c92b116742c36c02b51756\",\"item_name\":\"Chicken\n" +
+            "Curry \",\"item_description\":\"Ada Chicken la curry\",\"item_type\":\"Side\n" +
+            "Dish\",\"item_status\":2,\"toast_price\":250,\"restaurant\":\"572afada57f0798a07e1ee07\",\"__v\":0,\"active\":true,\"item_image_url_path\":[\"/home/toast2/toast/public/uploads/1472114407898Humus-&-Pita-.jpg\"],\"item_image_url\":[\"http://toastcentral.com/uploads/1472114407898Humus-&-Pita-.jpg\"]}]";
+    public ParseJson()
+    {
+
+    }
+}
---
+++ app/src/main/java/surana/sidhanth/com/guvitask/model/item.java
@@ -0,0 +1,15 @@
+package surana.sidhanth.com.guvitask.model;
+
+/**
+ * Created by sidhanth on 22/9/16.
+ */
+
+public class item {
+
+    String name;
+    String price;
+    String description;
+    String url;
+
+
+}
